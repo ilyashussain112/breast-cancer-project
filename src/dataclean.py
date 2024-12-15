98d94321dacded3c1aaa20e32fcea1f0a3aed487
abc <abc@gmail.com>
2024-12-15 18:23:19 +0500

    work done

--- src/dataclean.py
+++ src/dataclean.py
@@ -8,3 +8,14 @@ class clean:
     def __init__(self):
         pass
     
+    def drop_missing(self):
+        self.data = self.data.dropna()
+        return self.data
+     
+    def drop_duplicates(self):
+        self.data = self.data.drop_duplicates()
+        return self.data 
+    
+    def reset_indux(self):
+        self.data = self.data.reset_indux(drop = True)
+        return self.data
