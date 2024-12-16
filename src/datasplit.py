3417720289855a4306a2827d7957bf6a10be900b
abc <abc@gmail.com>
2024-12-16 16:09:57 +0500

    done

---
+++ src/datasplit.py
@@ -0,0 +1,13 @@
+from datatransform import Transform
+from sklearn.model_selection import train_test_split
+
+
+trans = Transform()
+x, y = trans.transform()
+class Splitter:
+    def __init__(self):
+        pass
+      
+    def splitter():
+        x_train, x_test, y_train, y_test = train_test_split(x,y , test_size = 0.2 , random_state=42)
+        return (x_train, x_test, y_train, y_test)
